c450ace55bedba9fed8a5fe7432ed777dd6a9965
Lauren Adams <lauren_adams3@baylor.edu>
2022-04-30 17:36:06 -0500

    fixed logger

--- groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/Business/Runner.java
+++ groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/Business/Runner.java
@@ -2,32 +2,26 @@ package edu.baylor.ecs.csi3471.groupProject.Business;
 
 import edu.baylor.ecs.csi3471.groupProject.UI.LoginPage;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Runner {
-	public static Logger logger = Logger.getLogger(Runner.class.getName());
+	public static Logger logger = Logger.getLogger(Timer.class.getName());
+
 	static {
 		try {
-			//InputStream configFile = Runner.class.getClassLoader().getResourceAsStream("logger.properties");
-			ClassLoader classLoader = Runner.class.getClass().getClassLoader();
-			InputStream configFile = classLoader.getResourceAsStream("logger.properties");
-			//InputStream configFile = Runner.class.getClass().getResourceAsStream("logger.properties");
-
+			InputStream configFile = Runner.class.getClassLoader().getResourceAsStream("logger.properties");
 			LogManager.getLogManager().readConfiguration(configFile);
 			configFile.close();
 		} catch (IOException ex) {
 			System.out.println("WARNING: Could not open configuration file");
-		    System.out.println("WARNING: Logging not configured");
-		} catch (NullPointerException ne) {
-			System.out.println("WARNING: Could not open configuration file");
-		    System.out.println("WARNING: Logging not configured");
+			System.out.println("WARNING: Logging not configured (console output only)");
 		}
 		logger.info("starting the app");
 	}
-	
 	public static User curUser;
 	public static void main(String[] args) {
 		LoginPage lp = new LoginPage();
