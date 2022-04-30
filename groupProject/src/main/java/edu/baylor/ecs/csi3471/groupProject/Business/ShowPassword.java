61c0f2e5351ad65c364dcca2a9f6c08ae7cd6bb9
Ryan Huntington <riot12301@gmail.com>
2022-04-30 18:15:37 -0500

    tests

--- groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/Business/ShowPassword.java
+++ groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/Business/ShowPassword.java
@@ -3,7 +3,10 @@ package edu.baylor.ecs.csi3471.groupProject.Business;
 import edu.baylor.ecs.csi3471.groupProject.UI.LoginForm;
 
 public class ShowPassword{
-	
+
+	/**
+	 * @param condition sets if the password is shown
+	 */
 	public void handlePassword(Integer condition) {
 		if(condition == 1) {
     		LoginForm.passwordField.setEchoChar((char)0);
