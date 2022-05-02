30b50d222ffbdf126d2a6ebbeb3399034e59aada
KateBurgess <85256159+KateBurgess@users.noreply.github.com>
2022-05-02 12:24:04 -0500

    Update RegisterForm.java
    Fixed the imports, added the color and font import

--- groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/UI/RegisterForm.java
+++ groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/UI/RegisterForm.java
@@ -3,6 +3,8 @@ package edu.baylor.ecs.csi3471.groupProject.UI;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
